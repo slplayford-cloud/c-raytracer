4d92808ec4e82f09daf71e70e3e773b5005eca72
Stephen Playford <slplayford@gmail.com>
2025-12-28 16:37:58 -0500

    update main.c and add raytracer.h

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+    Date: December 2025
+    Main file for C raytracer conversion project
+
+    @author Stephen Playford
+    @version 1.0
+*/
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int xResolution = 800;
+    int yResolution = 600;
+
+    //create the scene
+
+    //Render the scene into an object file
+
+    //save as a .png file
+
+	printf("main working\n");
+	return 0;
+}
